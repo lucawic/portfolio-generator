3db58b37f0855925dee76b7565f4a5d51041cfd9
lucawic <lucaswick@email.arizona.edu>
2021-08-07 10:07:40 -0700

    commit

--- app.js
+++ app.js
@@ -1,29 +1,20 @@
 const fs = require('fs');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+console.log(profileDataArgs);
+
 const [name, github] = profileDataArgs;
 
-const generatePage = (userName, githubName) => {
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content ="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
-    </head>
-    
-    <body>
-      <h1>${name}</h1>
-      <h2><a href="https://github.com/${github}">Github</a></h2>
-      </body>
-      </html>
-    `;
-};
-fs.writeFile('index.html', generatePage(name, github), err => {
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
     if (err) throw err;
 
-    console.log('Portfolio compelete! Check out index.html to see the output!');
+    console.log('Portfolio compelete! Check out index.html to see the ouput!');
 });
 
 /* console.log(profileDataArgs);
